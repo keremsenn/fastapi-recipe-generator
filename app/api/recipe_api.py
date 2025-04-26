498c8c5f8087e580e7ea4c61c5d769df113af205
keremsenn <senkerem949@gmail.com>
2025-04-26 20:33:26 +0300

    main page

--- app/api/recipe_api.py
+++ app/api/recipe_api.py
@@ -3,6 +3,9 @@ from app.model.recipe_model import RecipeResponse
 from fastapi import APIRouter
 
 router = APIRouter()
+@router.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 @router.post("/generate-recipe/", response_model=RecipeResponse)
 async def get_recipe(ingredients: list[str]):
