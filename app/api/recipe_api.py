4f0d27ec21c904e73b53335b0bf76c2483cb5ec0
keremsenn <senkerem949@gmail.com>
2025-04-26 21:05:08 +0300

    updates api

--- app/api/recipe_api.py
+++ app/api/recipe_api.py
@@ -3,8 +3,13 @@ from app.model.recipe_model import RecipeResponse
 from fastapi import APIRouter
 
 router = APIRouter()
-@router.get("/" ,methods=["GET", "HEAD"])
+@router.get("/")
 async def root():
+    return {"message": "Hello World"}
+
+# HEAD istekleri için özel handler
+@router.api_route("/", methods=["HEAD"])
+async def root_head():
     return {"message": "Hello World"}
 
 @router.post("/generate-recipe/", response_model=RecipeResponse)
